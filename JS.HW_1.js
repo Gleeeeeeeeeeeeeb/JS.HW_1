d71c9097b6179be9429da781810440e4477e5c1a
Gleeeeeeeeeeeeeb <klyuchnikovgleb@mail.ru>
2022-06-17 10:37:57 +0300

    file changed

--- JS.HW_1.js
+++ JS.HW_1.js
@@ -13,18 +13,14 @@ console.log(item_3+item_4);
 console.log(item_3*item_4);
 
 let item_5 = item_3;
-// console.log(item_5)
 
 let item_6 = 15;
-let item_6_type = Number(item_6);
-// console.log(typeof(item_6_type))
+let item_6_type = Number();
 console.log("item_6 == " + typeof(item_6));
 console.log("item_6_type == " + typeof(item_6_type));
 
 let item_7 = String(item_6)
-// console.log(typeof(item_7))
 let item_7_type = String();
-// console.log(typeof(item_7_type))
 
 console.log("item_7 == " + typeof(item_7))
 console.log("item_7_type == " + typeof(item_7_type))
@@ -32,3 +28,13 @@ console.log("item_7_type == " + typeof(item_7_type))
 let age_1 = 10;
 let age_2 = 18;
 let age_3 = 60;
+
+if(age_1 < age_2) {
+    console.log("You don't have access cause your age is " + age_1 + " It's less then ");
+}
+else if(age_1 >= age_2 , age_1 < age_3){
+    console.log("Welcome!");
+}
+else if(age_1 > age_3){
+    console.log("Keep calm and look Culture channel");
+}
